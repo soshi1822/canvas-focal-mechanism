e545f1151287482cfde3bf4cdaed38339043b411
soshi1822 <35869573+soshi1822@users.noreply.github.com>
2021-04-23 23:17:40 +0900

    Update type.d.ts

--- type.d.ts
+++ type.d.ts
@@ -38,9 +38,52 @@ interface TensorOptionC2 {
 
 export type TensorOption = TensorOptionC1 | TensorOptionC2;
 
+export interface TensorMatrix<Name extends string> {
+  name: Name;
+  eigenvalue: number;
+  vector: any;
+}
+
+export interface TensorStrikeDipRake<Name extends string> {
+  name: Name;
+  strike: number;
+  dip: number;
+  rake: number;
+}
+
 export function fms(tensor: Tensor, el: HTMLCanvasElement, options?: fmsOption): void;
 
 export class Tensor {
+  N: TensorMatrix<'N'>;
+  P: TensorMatrix<'P'>;
+  T: TensorMatrix<'T'>;
+
+  NP1: TensorStrikeDipRake<'NP1'>;
+  NP2: TensorStrikeDipRake<'NP2'>;
+
+  exponent: number;
+  fCLVD: number;
+  forceNormal: number;
+  forceStrikeSlip: number;
+  forceThrust: number;
+  magnitude: number;
+  matrix: {
+    data: number[];
+    m: number;
+    n: number;
+  };
+  moment: number;
+  momentLog10: number;
+  mtt: number;
+  mpp: number;
+  mrr: number;
+  mrt: number;
+  mrp: number;
+  mtp: number;
+  percentDC: number;
+  scale: number;
+  units: 'N-m';
+
   constructor(options: TensorOption);
 
   static fromStrikeDipRake(strike: number, dip: number, rake: number, moment: number): Tensor;
